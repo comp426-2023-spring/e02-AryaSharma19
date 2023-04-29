d1bffe112dad5bdbc594b798e40f420d1c27ed72
Arya Sharma <arya19@live.unc.edu>
2023-04-29 13:00:56 -0400

    added booleans to catch user information

--- public/scripts.js
+++ public/scripts.js
@@ -2,17 +2,40 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
+var rps = true
+var rpsls = false
+var opponent = false
+
+
 
 function playGame() {
     console.log("aha")
     fetch('/app/rps').then(response => response.json()).then(data => {
         console.log(data);
   });
-    
+
 }
 
 
-function showHideShots() {}
+function playingrps() {
+    rps = true;
+    rpsls = false;
+    console.log(rps);
+    console.log(rpsls);
+}
+
+function playingrpsls() {
+    rps = false;
+    rpsls = true;
+    console.log(rps);
+    console.log(rpsls);
+}
+
+
+function playingopponent() {
+    opponent = !opponent;
+    console.log(opponent);
+}
 
 function startOver() {
     console.log("Need to work on this")
